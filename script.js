9c1b8c4de86e64efc2b500624bc77e41e7d482d0
german <germanromo2006@gmail.com>
2025-10-05 13:03:00 +0200

    aaa

--- script.js
+++ script.js
@@ -11,12 +11,34 @@ const cargarPreguntas = async () => {
 }
 
 cargarPreguntas().then(() => {
-    crearDivPreguntas(preguntas)
-    });
+    for (let i = 0; i < preguntas.length; i++) {
+        crearDivPregunta(preguntas[i]); 
+    }
+});
 
+function crearDivPregunta(pregunta){  
+    //div
+    const divPregunta = document.createElement("div");
+    divPregunta.className = "contenedorPreguntas"; //css
 
+    //titulo
+    const titulo = document.createElement("h2");
+    titulo.className = "tituloPreguntas"; //css
+    titulo.innerHTML = pregunta.pregunta; 
 
+    divPregunta.appendChild(titulo);
+    document.getElementById("TODAS_LAS_PREGUNTAS").appendChild(divPregunta);
 
-function crearDivPreguntas(preguntas){
-
+    const DivRespuestas = document.createElement("div");
+    DivRespuestas .className = "DivRespuestas";
+    console.log(pregunta.respuestas)
+    pregunta.respuestas.forEach((texto, i) => {
+        const RadioButton = document.createElement("input")
+        RadioButton.type = "radio";
+        RadioButton.name =  "respuesta" + nPregunta;
+        RadioButton.id = "res" + i;
+        const textoRespuesta = document.createElement("label")
+        textoRespuesta.htmlFor = RadioButton.id; 
+        textoRespuesta.innerHTML = texto;   
+    });    
 }
